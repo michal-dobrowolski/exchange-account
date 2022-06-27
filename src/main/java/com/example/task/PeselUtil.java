97ede76322ce1484f419077a8dbd7bb24e915f39
mdobrowolski <braders12@gmail.com>
2022-06-27 16:19:55 +0200

    update pesel validation

---
+++ src/main/java/com/example/task/PeselUtil.java
@@ -0,0 +1,66 @@
+package com.example.task;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAccessor;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PeselUtil {
+
+    private static final Map<String, Integer> centuries = new HashMap<>(
+            Map.of(
+                    "0", 1900,
+                    "1", 1900,
+                    "2", 2000,
+                    "3", 2000,
+                    "4", 2100,
+                    "5", 2100,
+                    "6", 2200,
+                    "7", 2200,
+                    "8", 1800,
+                    "9", 1800
+            )
+    );
+
+   static public LocalDate getBirthday(String pesel) {
+        String splitByPairsRegex = "(?<=\\G.{2})";
+        String[] date = getDateFromPesel(pesel).split(splitByPairsRegex);
+        String centuryKey = getCenturyKey(date);
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .optionalStart()
+                .appendPattern("uuuu")
+                .optionalEnd()
+                .optionalStart()
+                .appendValueReduced(ChronoField.YEAR, 2, 2, centuries.get(centuryKey))
+                .optionalEnd()
+                .toFormatter();
+        TemporalAccessor parsedYear = formatter.parse(date[0]);
+        return LocalDate.of(
+                parsedYear.get(ChronoField.YEAR),
+                getMonth(date, centuryKey),
+                getDayOfMonth(date)
+        );
+    }
+
+    private static int getDayOfMonth(String[] date) {
+        return Integer.parseInt(date[2]);
+    }
+
+    private static int getMonth(String[] date, String centuryKey) {
+        return Integer.parseInt(centuryKey) % 2 == 0 ?
+                Integer.parseInt(date[1].substring(1, 2)) :
+                Integer.parseInt(date[1].substring(1, 2)) * 10;
+    }
+
+    private static String getCenturyKey(String[] date) {
+        return date[1].substring(0, 1);
+    }
+
+    private static String getDateFromPesel(String pesel) {
+        return pesel.substring(0, 6);
+    }
+
+}
--- src/main/java/com/example/task/domain/Account.java
+++ src/main/java/com/example/task/domain/Account.java
@@ -1,17 +1,13 @@
 package com.example.task.domain;
 
 import com.example.task.ApplicationException;
+import com.example.task.PeselUtil;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.ChronoField;
-import java.time.temporal.TemporalAccessor;
-import java.util.HashMap;
 import java.util.Map;
 
 @Setter
@@ -29,16 +25,6 @@ public class Account {
 
     private Map<String, SubAccount> subAccounts;
 
-    private static Map<String, Integer> baseValue = new HashMap<>(
-            Map.of(
-                    "0", 1900,
-                    "2", 2000,
-                    "4", 2100,
-                    "6", 2200,
-                    "8", 1800
-            )
-    );
-
     public Account(String id, String pesel, String lastName, String firstName, Map<String, SubAccount> subAccounts) {
         validateAge(pesel);
         this.id = id;
@@ -53,24 +39,8 @@ public class Account {
     }
 
     static void validateAge(String pesel) {
-        String REGEX = "(?<=\\G.{2})";
-        String[] date = pesel.substring(0, 6).split(REGEX);
-        String key = date[1].substring(0, 1);
+        LocalDate birthday = PeselUtil.getBirthday(pesel);
         LocalDate today = LocalDate.now();
-        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-                .optionalStart()
-                .appendPattern("uuuu")
-                .optionalEnd()
-                .optionalStart()
-                .appendValueReduced(ChronoField.YEAR, 2, 2, baseValue.get(key))
-                .optionalEnd()
-                .toFormatter();
-        TemporalAccessor temporalAccessor = formatter.parse(date[0]);
-        LocalDate birthday = LocalDate.of(
-                temporalAccessor.get(ChronoField.YEAR),
-                Integer.parseInt(date[1].substring(1, 2)),
-                Integer.parseInt(date[2])
-        );
         Period period = Period.between(birthday, today);
         if (period.getYears() < 18) {
             throw new ApplicationException("To set up an account, a person should be of legal age.");
