f1cee7acc558b453b02cdd80d6a1b7bba2c2ca6f
mdobrowolski <braders12@gmail.com>
2022-06-27 11:19:10 +0200

    update exception handling

--- src/main/java/com/example/task/GlobalExceptionHandler.java
+++ src/main/java/com/example/task/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.task;
 
 import com.mongodb.MongoWriteException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
-    @ExceptionHandler({ApplicationException.class, MongoWriteException.class})
+    @ExceptionHandler({ApplicationException.class, DuplicateKeyException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     Error handleException(Exception exception) {
+        if (exception instanceof DuplicateKeyException) {
+            return new Error("Error", "Account already exist with provided pesel number.");
+        }
         return new Error("Error", exception.getMessage());
     }
 
