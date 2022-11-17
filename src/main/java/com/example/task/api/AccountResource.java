d82ff84ba13be8578fd8e36d982a7921fa56b26d
mdobrowolski <braders12@gmail.com>
2022-11-17 18:05:37 +0100

    feat C

--- src/main/java/com/example/task/api/AccountResource.java
+++ src/main/java/com/example/task/api/AccountResource.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 @RestController
 @AllArgsConstructor
 public class AccountResource {
-
+//I tworza się konfilkty 
     private final AccountService accountService;
 //tutaj ktos cos domergowal
     @PostMapping("/accounts")
