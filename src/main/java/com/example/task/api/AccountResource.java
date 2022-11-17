ebb33cc37e318b61c50e8c08ed25a88b17cb1b10
mdobrowolski <braders12@gmail.com>
2022-11-17 18:05:20 +0100

    feat B

--- src/main/java/com/example/task/api/AccountResource.java
+++ src/main/java/com/example/task/api/AccountResource.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 public class AccountResource {
 
     private final AccountService accountService;
-
+//tutaj ktos cos domergowal
     @PostMapping("/accounts")
     public Mono<String> create(@Valid @RequestBody AccountRequest request) {
         return accountService.create(request.toDomain());
